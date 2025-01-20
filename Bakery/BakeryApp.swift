41ac6682de7b126ec690a8a4cae469775be89c9b
AbeerX <Abeeralali021@gmail.com>
2025-01-20 09:28:19 +0300

    Initial Commit

---
+++ Bakery/BakeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeryApp.swift
+//  Bakery
+//
+//  Created by Abeer on 20/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BakeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
